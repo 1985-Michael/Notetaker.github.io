27e6868c252ba0896bdcd08cdb582acc34b4507b
Michael Myers <anthonymyers1985@gmail.com>
2020-03-19 18:39:31 -0700

    changed server directory

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const notesData = require('./db/db.json');
 const app = express();
 const PORT = 3000;
 
-const Directory = path.join(__dirname, "/public");
+const Directory = path.join(__dirname, "Develop/public");
 
 app.use(express.json());
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./'));
 
-app.get("/notes", function(req, res) {
+
+app.get("api/notes", function(req, res) {
     res.sendFile(path.join(Directory, "notes.html"));
 
 });
@@ -21,13 +23,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/notes', (req, res) => {
+app.get('api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
     res.json(notesData);
-    notesData = fs.readFileSync("./db/db.json", "utf8");
+    notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
     notesData = JSON.parse(notesData);
     if (!err) {
         debugger;
@@ -41,12 +43,13 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
     const lastIdNum = notesData.length ? Math.max(...(notesData.map(note => note.id))) : 0;
-    const id = lastIdNum + 1;
+    let id;
+    id = lastIdNum + 1;
     notesData.push({ id, ...req.body });
     res.json(notesData.slice(-1));
-    notesData = fs.readFileSync("./db/db.json", "utf8");
+    notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
     notesData = JSON.parse(notesData);
-    fs.writeFile("./db/db.json", notesData, "utf8", function(err) {
+    fs.writeFile("./Develop/db/db.json", notesData, "utf8", function(err) {
         if (!err) {
             debugger;
         } else {
@@ -56,18 +59,21 @@ app.post('/api/notes', (req, res) => {
     });
 
     res.json(JSON.parse(notesData));
-
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    let note = notesData.find(({ id }) => id === JSON.parse(req.params.id));
+    let note;
+    note = notesData.find(({ id }) => id === JSON.parse(req.params.id));
     notesData.splice(notesData.indexOf(note), 1);
-    res.end("deleted the Note");
 
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+    let savedNotes;
+    savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
     let noteId = req.params.id;
-    let newId = 0;
+    let newId;
+    newId = 0;
     console.log(`Deleting note ${noteId}`);
+    res.end("deleted the Note");
     savedNotes = savedNotes.filter(currNote => {
         return currNote.id != noteID;
     })
@@ -77,8 +83,9 @@ app.delete('/api/notes/:id', (req, res) => {
         newID++;
     }
 
-    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(savedNotes));
     res.json(savedNotes);
 });
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () =>
+    console.log(`listening on port ${PORT}`));
